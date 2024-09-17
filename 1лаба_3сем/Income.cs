cb0622240683b9916b6a85a7aa57b8a9e33b5d18
Neyman39 <cacaoboards@mail.ru>
2024-09-18 00:43:35 +0300

    Добавьте файлы проекта.

---
+++ 1лаба_3сем/Income.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace _1_Laba_3sem
+{
+    class Income
+    {
+        public string Type { get; set; }
+        public DateTime Date { get; set; }
+        public string Source { get; set; }
+        public int Amount { get; set; }
+
+        public static string FromString(string input)
+        {
+            Income income = new Income();
+
+            string[] parts = input.Split(':');
+            string[] words = parts[0].Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+            string word = words[0];
+            if (words.Length > 1)
+            {
+                for (int i = 1; i < words.Length; i++)
+                {
+                    word += $" {words[i]}";
+                }
+            }
+            income.Type = word;
+
+            string[] properties = parts[1].Split(',');
+            foreach (string property in properties)
+            {
+                string[] kv = property.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+                string key = kv[0];
+                string value = kv[1];
+                if (kv.Length > 2)
+                {
+                    for (int i = 2; i < kv.Length; i++)
+                    {
+                        value += $" {kv[i]}";
+                    }
+                }
+
+                if (key == "дата")
+                {
+                    income.Date = DateTime.ParseExact(value, "yyyy.MM.dd", null);
+                }
+                else if (key == "источник")
+                {
+                    income.Source = value.Trim('"');
+                }
+                else if (key == "сумма")
+                {
+                    income.Amount = Convert.ToInt32(value);
+                }
+            }
+
+            return $"Тип: {income.Type}\nДата: {income.Date.ToString("yyyy.MM.dd")}\nИсточник: {income.Source}\nСумма: {income.Amount} р";
+        }
+    }
+
+}
---
+++ 1лаба_3сем/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Laba_3sem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string incomeStr = "Доходы компании: дата 2023.05.01, источник \"Сдача гаража в аренду\", сумма 5000";
+            Console.WriteLine(Income.FromString(incomeStr));
+        }
+    }
+
+}
