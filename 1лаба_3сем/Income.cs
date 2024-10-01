4d52027b8936ccf7d4e641429f4b8f56f6139772
Neyman39 <cacaoboards@mail.ru>
2024-10-01 20:12:32 +0300

    добавление абстрактного класса и изменение метода ObjectOutput

--- 1лаба_3сем/Income.cs
+++ 1лаба_3сем/Income.cs
@@ -1,100 +1,84 @@
 ﻿using System;
-using System.Text.RegularExpressions;
 using System.Globalization;
-using System.Linq;
-using System.Collections.Generic;
-using System.IO;
 
 namespace _1_Laba_3sem
 {
-    class Income
+
+    abstract class BaseStr
+    {
+        public virtual string LineOutput()
+        {
+            return "";
+        }
+    }
+    class Income : BaseStr
     {
         public string Type { get; set; }
         public DateTime Date { get; set; }
         public string Source { get; set; }
         public int Amount { get; set; }
 
-        public Income(string input)
+        public Income(string type, DateTime date, string source, int amount)
         {
-            var parts = MainString.ParseString(input);
-            Type = parts[0].Trim('"');
-            Date = DateTime.ParseExact(parts[1], "yyyy.MM.dd", null);
-            Source = parts[2].Trim('"');
-            Amount = Convert.ToInt32(parts[3]);
+            //var parts = MainString.ParseString(input);
+            Type = type;
+            Date = date;
+            Source = source;
+            Amount = amount;
+            //Type = parts[0].Trim('"');
+            //Date = DateTime.ParseExact(parts[1], "yyyy.MM.dd", null);
+            //Source = parts[2].Trim('"');
+            //Amount = Convert.ToInt32(parts[3]);
         }
 
-        public virtual string LineOutput()
+        public override string LineOutput()
         {
-            return $"{Type}: Дата: {Date.ToString("yyyy.MM.dd")}, Источник: {Source}, Сумма: {Amount}";
+            return $"{Type}:\n Дата: {Date.ToString("yyyy.MM.dd")}\n Источник: {Source}\n Сумма: {Amount} р\n";
         }
     }
-    class Operation : Income
+    class Operation : BaseStr
     {
+        public string Type { get; set; }
+        public DateTime Date { get; set; }
+        public string Source { get; set; }
+        public int Amount { get; set; }
         public string TypeOfOperation { get; set; }
 
-        public Operation(string input) : base(input)
+        public Operation(string type, DateTime date, string source, int amount, string operation)
         {
-            var parts = MainString.ParseString(input);
-            TypeOfOperation = parts[4].Trim('"');
+            //var parts = MainString.ParseString(input);
+            Type = type;
+            Date = date;
+            Source = source;
+            Amount = amount;
+            TypeOfOperation = operation;
         }
 
         public override string LineOutput()
         {
-            return $"{Type}: Дата: {Date.ToString("yyyy.MM.dd")}, Источник: {Source}, Сумма: {Amount}, Тип операции: {TypeOfOperation}";
+            return $"{Type}:\n Дата: {Date.ToString("yyyy.MM.dd")}\n Источник: {Source}\n Сумма: {Amount} р\n Тип операции: {TypeOfOperation}\n";
         }
     }
-    class IncomeFromAnIndividual : Income
+    class IncomeFromAnIndividual : BaseStr
     {
+        public string Type { get; set; }
+        public DateTime Date { get; set; }
+        public string Source { get; set; }
+        public int Amount { get; set; }
         public string SenderName { get; set; }
 
-        public IncomeFromAnIndividual(string input) : base(input)
+        public IncomeFromAnIndividual(string type, DateTime date, string source, int amount, string name)
         {
-            var parts = MainString.ParseString(input);
-            SenderName = parts[4].Trim('"');
+            //var parts = MainString.ParseString(input);
+            Type = type;
+            Date = date;
+            Source = source;
+            Amount = amount;
+            SenderName = name;
         }
         public override string LineOutput()
         {
-            return $"{Type}: Дата:  {Date.ToString("yyyy.MM.dd")} , Источник:  {Source} , Сумма:  {Amount}, Имя отправителя: {SenderName}";
-        }
-    }
-
-    class MainString
-    {
-        public string[] StrFromFiles(string filePath)
-        {
-            string fileContent = File.ReadAllText(filePath);
-            return fileContent.Split(new[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
-        }
-
-        public static List<string> ParseString(string input)
-        {
-            return Regex.Matches(input, @"[\""].+?[\""]|[^ ]+")
-                         .Cast<Match>()
-                         .Select(m => m.Value)
-                         .ToList();
-        }
-
-        public string ObjectOutput(string stroka)
-        {
-            var parts = ParseString(stroka);
-
-            if (parts[0].Trim('"') == "Доходы")
-            {
-                Income type1 = new Income(stroka);
-                 return type1.LineOutput();
-            }
-            else if (parts[0].Trim('"') == "Доходы компании")
-            {
-                Operation type2 = new Operation(stroka);
-                return type2.LineOutput();
-            }
-            else if (parts[0].Trim('"') == "Доходы физ.лица")
-            {
-                IncomeFromAnIndividual type3 = new IncomeFromAnIndividual(stroka);
-                return type3.LineOutput();
-            }
-            else
-                return "Возникла ошибка";
+            return $"{Type}:\n Дата: {Date.ToString("yyyy.MM.dd")}\n Источник: {Source}\n Сумма: {Amount} р\n Имя отправителя: {SenderName}\n";
         }
     }
 }
--- 1лаба_3сем/Program.cs
+++ 1лаба_3сем/Program.cs
@@ -11,7 +11,7 @@ namespace _1_Laba_3sem
     {
         static void Main(string[] args)
         {
-            Console.OutputEncoding = Encoding.UTF8;
+            //Console.OutputEncoding = Encoding.UTF8;
             MainString mainstr = new MainString();
 
             string income1 = "\"Доходы компании\" 2023.05.01 \"Сдача гаража в аренду\" 35000 \"Перевод на сбер\"";
@@ -19,17 +19,19 @@ namespace _1_Laba_3sem
             string income3 = "\"Доходы\" 2023.05.04 \"Зарплата\" 120500";
             string income4 = "\"Доходы физ.лица\" 2023.05.04 \"Возврат долга\" 1500 \"Иванов И.И.\"";
 
+            //Console.WriteLine(mainstr.ObjectOutput(income2).LineOutput());
+
             Console.WriteLine("Вывод из строк");
             string[] provera = new string[] { income1, income2, income3, income4 };
             foreach (string linestr in provera)
             {
-                Console.WriteLine(mainstr.ObjectOutput(linestr));
+                Console.WriteLine(mainstr.ObjectOutput(linestr).LineOutput());
             }
 
             Console.WriteLine("\nВывод из файла");
-            foreach (string linefile in mainstr.StrFromFiles("2.txt"))
+            foreach (string linefile in mainstr.StrFromFiles("types.txt"))
             {
-                Console.WriteLine(mainstr.ObjectOutput(linefile));
+                Console.WriteLine(mainstr.ObjectOutput(linefile).LineOutput());
             }
         }
     }
